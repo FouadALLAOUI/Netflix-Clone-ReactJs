aaaffe233bf32e4a26449a8fbcebf5e77a98a39c
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-03-16 12:18:16 +0000

    create the project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDK2Lo4FIDIpml2biCAay0UELJgScOv24E",
+    authDomain: "netflix-clone-project-fc42e.firebaseapp.com",
+    projectId: "netflix-clone-project-fc42e",
+    storageBucket: "netflix-clone-project-fc42e.appspot.com",
+    messagingSenderId: "689748593836",
+    appId: "1:689748593836:web:12f1ac8b5c54407a67a309"
+  };
