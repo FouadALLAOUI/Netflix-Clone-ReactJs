6652b888359075c6917dded15db033ec43110953
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-03-16 23:38:45 +0000

    Preparing the authentitication with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyDK2Lo4FIDIpml2biCAay0UELJgScOv24E",
     authDomain: "netflix-clone-project-fc42e.firebaseapp.com",
@@ -6,3 +10,12 @@ const firebaseConfig = {
     messagingSenderId: "689748593836",
     appId: "1:689748593836:web:12f1ac8b5c54407a67a309"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {auth};
+export default db;
+
+
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import '../styles/SignupScreen.css'
+import { auth } from '../firebase';
 
 function SignupScreen() {
+  
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) =>{
+    e.preventDefault();
+    
+    auth.createUserWithEmailAndPassword()
+    
+  }
+  const signIn = (e) =>{
+    e.preventDefault();
+  }
+
   return (
     <div className='signupScreen'>
       <form>
         <h1>Sign In</h1>
-        <input placeholder='Email' type='email' />
-        <input placeholder='Password' type='password' />
-        <button type='submit'>Sign In</button>
+        <input ref={emailRef} placeholder='Email' type='email' />
+        <input ref={passwordRef} placeholder='Password' type='password' />
+        <button type='submit' onClick={signIn}>
+          Sign In
+        </button>
         
         <h4> 
-          <span className='signupScreen_gray'>New to Netflix?</span>
-          Sign Up now.
+          <span className='signupScreen_gray'>New to Netflix? </span> 
+          <span className='signupScreen_link' onClick={register}>
+            Sign Up now.
+          </span>
         </h4>
 
       </form>
@@ -22,3 +41,4 @@ function SignupScreen() {
 }
 
 export default SignupScreen
+
