d4948f4b4b79deab5f73c8ef6030334006037123
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-03-16 14:56:06 +0000

    Add Router in the the App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
-
 import './App.css';
 import HomeScreen from './HomeScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <HomeScreen/>
-
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Router>
+      
       
     </div>
   );
